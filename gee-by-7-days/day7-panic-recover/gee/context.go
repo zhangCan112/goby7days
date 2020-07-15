14e176dbef22f5e819b81579ec6e475f697f476e
zhangcan <zhangcan@xiaoshouyi.com>
2020-07-15 08:04:25 +0800

    init catch-by-7-days & day1-part1

---
+++ gee-by-7-days/day7-panic-recover/gee/context.go
@@ -0,0 +1,106 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// H rename for router
+type H map[string]interface{}
+
+// Context 记录一次网络请求完整上下文的闭环
+type Context struct {
+	Writer     http.ResponseWriter
+	Req        *http.Request
+	Path       string
+	Method     string
+	Params     map[string]string
+	StatusCode int
+	// middleware
+	handlers []HandlerFunc
+	index    int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+		index:  -1,
+	}
+}
+
+// Param 获取URL的path参数
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
+
+// Next 顺序执行中间件
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c)
+	}
+}
+
+// PostForm 获取表单数据中的某个值
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+// Query 查询Query参数中的某个值
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+// Status  设置StatusCode
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+// SetHeader 设置头部
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// String 设置text格式的内容
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+// JSON 设置Json格式的内容
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+// Data 设置Data格式的内容
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+// HTML 设置HTML格式的内容
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
+
+// Fail 返回报错信息
+func (c *Context) Fail(code int, err string) {
+	c.index = len(c.handlers)
+	c.JSON(code, H{"message": err})
+}
